2ac8a00900065d822fc360529c045da0c49789d9
xiaohua <zuwias7@gmail.com>
2024-02-25 11:42:49 +0800

    fix

--- xuecheng-plus-gateway/src/test/java/org/xiaohuadev/GatewayApplicationTests.java
+++ xuecheng-plus-gateway/src/test/java/org/xiaohuadev/GatewayApplicationTests.java
@@ -1,13 +1,13 @@
 package org.xiaohuadev;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayApplicationTests {
+public class GatewayApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    public void name() {
 
+    }
 }
